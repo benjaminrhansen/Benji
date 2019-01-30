ec75f38f4a0f9659b16d6722098be9e166da8f89
benjamminhansen <43420064+benjamminhansen@users.noreply.github.com>
2019-01-30 09:36:39 -0700

    sample.erl
    Added Lambda

--- sample.erl
+++ sample.erl
@@ -5,3 +5,9 @@ divide_by_all(Result,0) ->
     Result;
 divide_by_all(Dividend,Count) when Count > 0 ->
     divide_by_all(Dividend/Count,Count-1).
+
+do_this(Func) ->
+    Func().
+
+start() ->
+    do_this(fun() -> 2 + 2 end).
